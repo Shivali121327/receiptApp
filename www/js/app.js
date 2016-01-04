0f8e6e02ac9a9922b6dedff2e55b746dc918de9b
Shivali Chansoriya <chansoriyashivali@gmail.com>
2016-01-04 19:21:49 +0530

    add a create icon and basic home page

--- www/js/app.js
+++ www/js/app.js
@@ -49,11 +49,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
-    .state('app.playlists', {
-      url: '/playlists',
+    .state('app.home', {
+      url: '/home',
       views: {
         'menuContent': {
-          templateUrl: 'templates/playlists.html',
+          templateUrl: 'templates/home.html',
           controller: 'PlaylistsCtrl'
         }
       }
@@ -69,5 +69,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/home');
 });
