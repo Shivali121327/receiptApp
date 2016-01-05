5054e91344382c83e346e529af404260959afcb0
Shipra Sinha <shipra.juet@gmail.com>
2016-01-05 11:10:24 +0530

    Create login page

--- www/js/app.js
+++ www/js/app.js
@@ -50,7 +50,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     })
 
-
   .state('app.display', {
       url: '/display',
       views: {
@@ -59,7 +58,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
+
  
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/myReceipts');
+  $urlRouterProvider.otherwise('/index');
 });
